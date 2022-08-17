1c1f19006e7fac9c60a28a96629f079c8b8b0b5c
Abhirup <abhidatta146@gmail.com>
2022-08-17 21:36:02 +0530

    ResizeComponent

---
+++ src/ResizeComponent.tsx
@@ -0,0 +1,23 @@
+import { useEffect, useState } from "react";
+
+const ResizeComponent = ({name} : {name: string})=>{
+   const  [innerWidth, setInnerWidth] = useState(0);
+
+   const handleResize = ()=>{
+    setInnerWidth(window.innerWidth);
+   }
+
+   useEffect(()=>{
+    window.addEventListener('resize', handleResize);
+
+    return ()=>{
+      window.addEventListener('resize', handleResize);
+    }
+   },[]);   
+
+   return (
+    <div>Inner Width is {innerWidth} and name is {name}</div>
+   )
+}
+
+export default ResizeComponent;
